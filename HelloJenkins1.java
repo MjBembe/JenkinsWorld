de79c0150f9edf0102ccadfc0820fff3b07b7539
MjBembe <61251576+MjBembe@users.noreply.github.com>
2020-04-14 09:46:46 +0200

    Update HelloJenkins1.java

--- HelloJenkins1.java
+++ HelloJenkins1.java
@@ -1,7 +1,7 @@
 public class HelloJenkins1 {
 	public static void main(String[] args) {
 
-	System.out.println("Guess what? This was built from Git SCM system automatically. ");
+	System.out.println("Guess what, This was built from Git SCM system automatically. ");
 
 	}
 }
